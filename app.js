5391064156b9b3be31c6229653df5bb7ef70e8c1
JohnWestermeyer <johnwestermeyer@gmail.com>
2020-12-01 16:20:13 -0800

    CSS gen function added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
+const cssPath = path.join(OUTPUT_DIR, "style.css");
 
 const renderer = require("./lib/htmlRenderer");
 const Employee = require("./lib/Employee");
@@ -109,7 +110,7 @@ const followUp = [
         name: "cont"
     }
 ]
-
+//the main show
 function init() {
     inquirer.prompt(questions).then((response) => {
         if(response.role === "Manager"){            
@@ -133,8 +134,9 @@ function init() {
             else{
                 try {
                     let text = renderer.render(employees);
-                    checkFolder();                    
-                    writeToFile(outputPath, text)
+                    checkFolder(); 
+                    checkCSS();                   
+                    writeToFile(outputPath, text);
                 } catch (error) {
                     console.error(error);
                 }
@@ -151,31 +153,20 @@ function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) =>
     err ? console.error(err) : console.log('Success!'));
 }
-//checking if output folder exists and if not creates it :3
+//checking if output folder exists and if not creates it
 function checkFolder(){
     if (!fs.existsSync(OUTPUT_DIR)) {
         fs.mkdirSync(OUTPUT_DIR);
     }
 }
+//checking if external CSS exist and if not creates it
+function checkCSS(){
+    if(!fs.existsSync(cssPath)){
+        writeToFile(cssPath, /* CSS GOES HERE */);
+    }
+}
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// :3 :3 :3
+//johnwestermeyer.github.io
 
 init();
